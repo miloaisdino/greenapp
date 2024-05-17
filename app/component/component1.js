1564635b7ce0e19c56a1c157e22b812093ed5020
keith <keithonghx@gmail.com>
2024-05-17 14:15:15 +0800

    updated file structure

---
+++ app/component/component1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Component1 = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample component.</p>
+    </div>
+  );
+};
+
+export default Component1;
