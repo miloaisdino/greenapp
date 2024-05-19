b1cda54438057a31ca829420df178a1d3f0f6955
miloaisdino <miloaisdino@gmail.com>
2024-05-19 11:59:53 +0800

    sample db access implementation

--- pages/api/health-check.js
+++ pages/api/health-check.js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'ok' });
+import { createClient } from '@supabase/supabase-js'
+
+export default async function handler(req, res) {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    const supabase = createClient(supabaseUrl, supabaseAnonKey);
+    const { data, error} = await supabase.from("test").select("*");
+    res.status(200).json({ message: data });
 }
