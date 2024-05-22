067d34dd6018eece248b99def6f956fcf91c7513
keith <keithonghx@gmail.com>
2024-05-22 20:14:11 +0800

    removed user from route (making it a grouped route)

--- app/redemption/page.js
+++ app/redemption/page.js
@@ -1,10 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { fetchCurrentUser } from "../component/fetchUser";
 import apiLinks from "../pages/api";
 import Header from "../component/header";
+import { toast } from "react-toastify";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -12,46 +13,10 @@ function classNames(...classes) {
 
 export default function Redemption() {
   const [user, setUser] = useState(null);
-  const [clients, setClients] = useState([
-    {
-      id: 1,
-      name: "Tuple",
-      imageUrl: "https://tailwindui.com/img/logos/48x48/tuple.svg",
-      lastInvoice: {
-        date: "December 13, 2022",
-        dateTime: "2022-12-13",
-        amount: "$2,000.00",
-        status: "Overdue",
-      },
-    },
-    {
-      id: 2,
-      name: "SavvyCal",
-      imageUrl: "https://tailwindui.com/img/logos/48x48/savvycal.svg",
-      lastInvoice: {
-        date: "January 22, 2023",
-        dateTime: "2023-01-22",
-        amount: "$14,000.00",
-        status: "Paid",
-      },
-    },
-    {
-      id: 3,
-      name: "Reform",
-      imageUrl: "https://tailwindui.com/img/logos/48x48/reform.svg",
-      lastInvoice: {
-        date: "January 23, 2023",
-        dateTime: "2023-01-23",
-        amount: "$7,600.00",
-        status: "Paid",
-      },
-    },
-  ]);
-  const [formDetails, setFormDetails] = useState({
-    image_url: "",
-    description: "",
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
   });
-  const products = [
+  const rewards = [
     {
       id: 1,
       name: "Zip Tote Basket",
@@ -109,6 +74,7 @@ export default function Redemption() {
     },
     // More products...
   ];
+  const [availableRewards, setAvailableRewards] = useState([]); // Add in rewards later
 
   const handleInputChange = (e) => {
     setFormValues({
@@ -144,35 +110,58 @@ export default function Redemption() {
       const currentUser = await fetchCurrentUser();
       setUser(currentUser);
     }
+    fetch(`${apiLinks.main}/rewards`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response from the server
+        console.log(data);
+        // Update the rewards state with the fetched data
+        setAvailableRewards(data);
+      })
+      .catch((error) => {
+        // Handle any errors
+        console.error(error);
+        toast.error(
+          "An error occurred with fetching rewards. Please try again."
+        );
+      });
 
     getUser();
   }, []); //Add in userid later
+
+  const handleAddToCart = (reward) => {
+    setShoppingCart((prevCart) => ({
+      ...prevCart,
+      items: [...prevCart.items, reward],
+    }));
+    console.log(shoppingCart);
+  };
 
   return (
     <>
       <Header />
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-xl font-bold text-gray-900">Available Rewards</h2>
+          <header className="text-xl font-bold text-gray-900">
+            Available Rewards
+          </header>
 
           <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <div key={product.id}>
+            {rewards.map((reward) => (
+              <div key={reward.id}>
                 <div className="relative">
                   <div className="relative h-72 w-full overflow-hidden rounded-lg">
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={reward.imageSrc}
+                      alt={reward.imageAlt}
                       className="h-full w-full object-cover object-center"
                     />
                   </div>
                   <div className="relative mt-4">
                     <h3 className="text-sm font-medium text-gray-900">
-                      {product.name}
+                      {reward.name}
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      {product.color}
-                    </p>
+                    <p className="mt-1 text-sm text-gray-500">{reward.color}</p>
                   </div>
                   <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
                     <div
@@ -180,24 +169,23 @@ export default function Redemption() {
                       className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
                     />
                     <p className="relative text-lg font-semibold text-white">
-                      {product.price}
+                      {reward.price}
                     </p>
                   </div>
                 </div>
                 <div className="mt-6">
-                  <a
-                    href={product.href}
-                    className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200"
+                  <button
+                    onClick={() => handleAddToCart(reward)}
+                    className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200 w-full"
                   >
-                    Add to bag<span className="sr-only">, {product.name}</span>
-                  </a>
+                    Add to bag<span className="sr-only">, {reward.name}</span>
+                  </button>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-      )
     </>
   );
 }
