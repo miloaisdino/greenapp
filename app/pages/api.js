5018c1771a91b7b2af8c64f8041270d7395b1dec
miloaisdino <miloaisdino@gmail.com>
2024-05-30 01:11:04 +0800

    update env var

--- app/pages/api.js
+++ app/pages/api.js
@@ -1,6 +1,6 @@
 const apiLinks = {
   // API link for the production environment
-  main: process.env.NEXT_PUBLIC_URL,
+  main: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000/',
 };
 
 // Export the API links
