4683a996d3fa87afa0e429812aba00eb8e84d9a9
miloaisdino <miloaisdino@gmail.com>
2024-05-30 01:31:15 +0800

    fix env var

--- app/pages/api.js
+++ app/pages/api.js
@@ -1,6 +1,15 @@
+let url;
+if (process.env.NEXT_PUBLIC_HOST) {
+  url = process.env.NEXT_PUBLIC_HOST;
+} else if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+  url = 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL;
+} else {
+  url = 'http://localhost:3000/';
+}
+
 const apiLinks = {
   // API link for the production environment
-  main: 'https://'+process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000/',
+  main: url,
 };
 
 // Export the API links
