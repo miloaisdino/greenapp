88a5832ef7027af2e215ba77f267514b4c65875c
keith <keithonghx@gmail.com>
2024-06-13 16:32:36 +0800

    minor display fix for redemption page

--- app/(with-auth)/redemption/page.js
+++ app/(with-auth)/redemption/page.js
@@ -4,10 +4,11 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { fetchCurrentUser } from "../../component/fetchUser";
 import apiLinks from "../../pages/api";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ConfirmationModal from "./confirmationModal";
 import { createClient } from "@/lib/supabase/component";
+import { CldImage } from "next-cloudinary";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -20,7 +21,6 @@ export default function Redemption() {
   const [showModal, setShowModal] = useState(false);
   const supabase = createClient();
 
-
   const toggleModal = (e) => {
     setShowModal(!showModal);
   };
@@ -32,18 +32,21 @@ export default function Redemption() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({'reward_id': selectedItem.reward_id}),
+      body: JSON.stringify({ reward_id: selectedItem.reward_id }),
     })
       .then((response) => response.json())
       .then((result) => {
         // Handle the response from the server
         //console.log(result);
-        if (result.error){
+        if (result.error) {
           toast.error(result.error);
         } else {
           toast.success(
-              <div>Redemption successful!
-                <br />You will receive an email within <b>24 hours</b> with details</div>
+            <div>
+              Redemption successful!
+              <br />
+              You will receive an email within <b>24 hours</b> with details
+            </div>
           );
           //update client side balances
           fetchUser();
@@ -68,9 +71,7 @@ export default function Redemption() {
       setUserData(data);
     } catch (error) {
       console.error(error);
-      toast.error(
-          "An error occurred while fetching user data."
-      );
+      toast.error("An error occurred while fetching user data.");
     }
   };
 
@@ -116,56 +117,65 @@ export default function Redemption() {
         )}
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <header className="flex justify-between items-center">
-            <div className="text-xl font-bold text-gray-900">Available Rewards</div>
-            <div className="float-right pb-1 text-base text-gray-500">Current Points: {userData.balances?.current_points}</div>
+            <div className="text-xl font-bold text-gray-900">
+              Available Rewards
+            </div>
+            <div className="float-right pb-1 text-base text-gray-500">
+              Current Points: {userData.balances?.current_points}
+            </div>
           </header>
 
           <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
             {availableRewards.map((reward) => (
-                <div key={reward.reward_id}>
-                  <div className="relative">
-                    <div className="relative h-72 w-full overflow-hidden rounded-lg">
-                      <img
-                          src={reward.image_url || "placeholderImage.svg"}
-                          alt={"Cannot find"}
-                          className="h-full w-full object-cover object-center"
-                      />
-                    </div>
-                    <div className="relative mt-4">
-                      <h3 className="text-sm font-medium text-gray-900">
-                        {reward.name}
-                      </h3>
-                    </div>
-                    <div
-                        className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
-                      <div
-                          aria-hidden="true"
-                          className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                      />
-                      <p className="relative text-lg font-semibold text-white">
-                        {reward.points_cost} points
-                      </p>
-                    </div>
+              <div key={reward.reward_id}>
+                <div className="relative">
+                  <div className="relative h-72 w-full overflow-hidden rounded-lg">
+                    <CldImage
+                      src={reward.image_url}
+                      alt={reward.name || "Reward not available"}
+                      fill
+                      loading="lazy"
+                      sizes="(max-width: 768px) 50vw, (max-width: 1200px) 25vw, 25vw"
+                    />
                   </div>
-                  <div className="mt-6">
-                    <button
-                        onClick={() => {
-                          setSelectedItem({
-                            'reward_id': reward.reward_id,
-                            'name': reward.name,
-                            'price': reward.points_cost
-                          });
-                          toggleModal();
-                        }}
-                        className="relative flex items-center justify-center rounded-md border
-                        border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900
-                        hover:bg-gray-200 w-full disabled:cursor-not-allowed disabled:hover:bg-gray-100"
-                        disabled={userData.balances?.current_points < reward.points_cost}
-                    >
-                      {userData.balances?.current_points < reward.points_cost ? "Insufficient Points" : "Redeem"}
-                    </button>
+                  <div className="relative mt-4">
+                    <h3 className="text-sm font-medium text-gray-900">
+                      {reward.name}
+                    </h3>
+                  </div>
+                  <div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
+                    <div
+                      aria-hidden="true"
+                      className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                    />
+                    <p className="relative text-lg font-semibold text-white">
+                      {reward.points_cost} points
+                    </p>
                   </div>
                 </div>
+                <div className="mt-6">
+                  <button
+                    onClick={() => {
+                      setSelectedItem({
+                        reward_id: reward.reward_id,
+                        name: reward.name,
+                        price: reward.points_cost,
+                      });
+                      toggleModal();
+                    }}
+                    className="relative flex items-center justify-center rounded-md border
+                        border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900
+                        hover:bg-gray-200 w-full disabled:cursor-not-allowed disabled:hover:bg-gray-100"
+                    disabled={
+                      userData.balances?.current_points < reward.points_cost
+                    }
+                  >
+                    {userData.balances?.current_points < reward.points_cost
+                      ? "Insufficient Points"
+                      : "Redeem"}
+                  </button>
+                </div>
+              </div>
             ))}
           </div>
         </div>
